60ea23978d92d7af18247dfb60ba96e7829f1b68
nasoooor <nasoooor29@gmail.com>
2026-06-24 00:05:51 +0300

    move things around

--- internal/config/http.go
+++ internal/config/http.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 	"time"
 	"wget/internal/utils"
-
-	"golang.org/x/time/rate"
 )
 
-func NewHTTPClient(conf *Options) (*http.Client, error) {
+type CustomHttpClient struct {
+	*http.Client
+}
+
+func NewHTTPClient(conf *Options) (*CustomHttpClient, error) {
 	rateLimit, err := utils.ParseRateLimit(conf.RateLimit)
 	if err != nil {
 		slog.Error("", "err", err)
@@ -24,56 +26,10 @@ func NewHTTPClient(conf *Options) (*http.Client, error) {
 	}
 	slog.Debug("Timeout set", "seconds", conf.Timeout)
 
-	return &http.Client{
-		Timeout:   time.Duration(conf.Timeout) * time.Second, // important: large downloads should not timeout
-		Transport: NewRateLimitTransport(rateLimit),
+	return &CustomHttpClient{
+		Client: &http.Client{
+			Timeout:   time.Duration(conf.Timeout) * time.Second, // important: large downloads should not timeout
+			Transport: NewRateLimitTransport(rateLimit),
+		},
 	}, nil
-}
-
-type RateLimitTransport struct {
-	Base        http.RoundTripper
-	BytesPerSec int64
-}
-
-func NewRateLimitTransport(bytesPerSec int64) *RateLimitTransport {
-	return &RateLimitTransport{
-		BytesPerSec: bytesPerSec,
-	}
-}
-
-func (t *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	base := t.Base
-	if base == nil {
-		base = http.DefaultTransport
-	}
-
-	resp, err := base.RoundTrip(req)
-	if err != nil {
-		return nil, err
-	}
-	totalSize := resp.ContentLength
-	isUnkownSize := totalSize <= 0
-	if totalSize <= 0 {
-		slog.Debug("Content length is unknown, will use spinner for progress indication")
-	}
-
-	var limiter *rate.Limiter
-	if t.BytesPerSec > 0 {
-		burst := int(t.BytesPerSec)
-		if burst < 1 {
-			burst = 1
-		}
-		limiter = rate.NewLimiter(rate.Limit(t.BytesPerSec), burst)
-	}
-
-	resp.Body = &customReader{
-		ReadCloser:     resp.Body,
-		limiter:        limiter,
-		maxChunkBytes:  int(t.BytesPerSec),
-		totalSizeBytes: totalSize,
-		currentBytes:   0,
-		isUnknownSize:  isUnkownSize,
-	}
-
-	return resp, nil
 }
--- internal/config/rate.go
+++ internal/config/rate.go
@@ -1,119 +1,56 @@
 package config
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"os"
-	"strings"
-	"sync"
-	"wget/internal/utils"
+	"log/slog"
+	"net/http"
 
 	"golang.org/x/time/rate"
 )
 
-var spinnerFrames = []string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴"}
-
-type customReader struct {
-	io.ReadCloser
-	limiter        *rate.Limiter
-	maxChunkBytes  int
-	totalSizeBytes int64
-	currentBytes   int64
-	isUnknownSize  bool
-	spinnerIndex   int
-	finishOnce     sync.Once
+type RateLimitTransport struct {
+	Base        http.RoundTripper
+	BytesPerSec int64
 }
 
-func (r *customReader) Read(p []byte) (int, error) {
-	if r.maxChunkBytes > 0 && len(p) > r.maxChunkBytes {
-		p = p[:r.maxChunkBytes]
+func NewRateLimitTransport(bytesPerSec int64) *RateLimitTransport {
+	return &RateLimitTransport{
+		BytesPerSec: bytesPerSec,
+	}
+}
+
+func (t *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	base := t.Base
+	if base == nil {
+		base = http.DefaultTransport
 	}
 
-	if r.limiter != nil {
-		if err := r.limiter.WaitN(context.Background(), len(p)); err != nil {
-			return 0, err
+	resp, err := base.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+	totalSize := resp.ContentLength
+	isUnkownSize := totalSize <= 0
+	if totalSize <= 0 {
+		slog.Debug("Content length is unknown, will use spinner for progress indication")
+	}
+
+	var limiter *rate.Limiter
+	if t.BytesPerSec > 0 {
+		burst := int(t.BytesPerSec)
+		if burst < 1 {
+			burst = 1
 		}
+		limiter = rate.NewLimiter(rate.Limit(t.BytesPerSec), burst)
 	}
 
-	n, err := r.ReadCloser.Read(p)
-	if n > 0 {
-		r.currentBytes += int64(n)
-		r.render(false)
-	}
-	if err == io.EOF {
-		r.finish()
-	}
-	return n, err
-}
-
-func (r *customReader) Close() error {
-	r.finish()
-	return r.ReadCloser.Close()
-}
-
-func (r *customReader) finish() {
-	r.finishOnce.Do(func() {
-		r.render(true)
-		fmt.Fprintln(os.Stderr)
-	})
-}
-
-func (r *customReader) render(done bool) {
-	if r.isUnknownSize {
-		r.renderSpinner(done)
-		return
-	}
-	r.renderBar(done)
-}
-
-func (r *customReader) renderBar(done bool) {
-	const barWidth = 28
-	total := r.totalSizeBytes
-	if total <= 0 {
-		total = r.currentBytes
+	resp.Body = &customReader{
+		ReadCloser:     resp.Body,
+		limiter:        limiter,
+		maxChunkBytes:  int(t.BytesPerSec),
+		totalSizeBytes: totalSize,
+		currentBytes:   0,
+		isUnknownSize:  isUnkownSize,
 	}
 
-	percent := 0.0
-	if total > 0 {
-		percent = float64(r.currentBytes) / float64(total)
-		if percent > 1 {
-			percent = 1
-		}
-	}
-
-	filled := int(percent * barWidth)
-	if filled > barWidth {
-		filled = barWidth
-	}
-	bar := strings.Repeat("█", filled) + strings.Repeat("░", barWidth-filled)
-
-	status := "Downloading"
-	if done {
-		status = "Done"
-	}
-
-	line := fmt.Sprintf("%s [%s] %6.2f%% (%s/%s)", status, bar, percent*100, utils.FormatBytes(r.currentBytes), utils.FormatBytes(total))
-	r.writeLine(line, done)
-}
-
-func (r *customReader) renderSpinner(done bool) {
-	frame := spinnerFrames[r.spinnerIndex%len(spinnerFrames)]
-	r.spinnerIndex++
-
-	status := "Downloading"
-	if done {
-		status = "Done"
-	}
-
-	line := fmt.Sprintf("%s %s %s", status, frame, utils.FormatBytes(r.currentBytes))
-	r.writeLine(line, done)
-}
-
-func (r *customReader) writeLine(line string, done bool) {
-	if done {
-		fmt.Fprintf(os.Stderr, "\r\x1b[2K%s\n", line)
-		return
-	}
-	fmt.Fprintf(os.Stderr, "\r\x1b[2K%s", line)
+	return resp, nil
 }
